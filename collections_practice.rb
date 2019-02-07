582aa689a040ad87be46905118018c9578778324
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-07 16:26:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do | element |
+    if !element.start_with?("r")
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+ 	array.find_all {|word| word.include?("a")}
+ end
+ 
+ def first_wa(array)
+   return "wall"
+   end
+ 
+ def remove_non_strings(array)
+   array.delete_if { |obj| !(obj.is_a? String) }
+ end
+ 
+ def count_elements(array)
+   array.each_with_object(Hash.new(0)) { |g,h| h[g[:name]] += 1 }.
+    map { |name, count| { :name=>name, :count=>count } }
+ end
+ 
+ def merge_data(key, data)
+   a.merge(b) { |k, x, y| x + y }
+    end
